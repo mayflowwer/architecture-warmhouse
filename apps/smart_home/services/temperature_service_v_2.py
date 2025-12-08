fa91189a7259b3c98f6d8b152c6948b62f9d4152
mayflowwer <mayflowwer@gmail.com>
2025-12-08 07:49:57 +0200

    temperature_service_v_2.py added endpoint for fetching temp for sensor id

--- apps/smart_home/services/temperature_service_v_2.py
+++ apps/smart_home/services/temperature_service_v_2.py
@@ -10,14 +10,15 @@ class TemperatureResponse(BaseModel):
     sensorId: str
     temperature: float
 
+@app.get("/temperature/{sensor_id}", response_model=TemperatureResponse)
+def get_temperature_by_sensor_id(sensor_id):
+    temperature = random.randint(1, 100)
+    return TemperatureResponse(location="default", sensorId=sensor_id, temperature=temperature)
 
 @app.get("/temperature", response_model=TemperatureResponse)
-def get_temperature(location: str = Query(..., description="Name of the room")):
-    """
-    Получает температуру по location. 
-    Если location не распознан, sensorId = "0".
-    """
-    # Присваиваем sensorId по location
+def get_temperature(
+    location: str = Query(..., description="Name of the room"),   
+    ):
     if location == "Living Room":
         sensorId = "1"
     elif location == "Bedroom":
